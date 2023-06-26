349325c6b31a2d54f1f7ba411bc9b3410f1af807
Enterprice <meshushkevich@gmail.com>
2023-06-26 10:57:42 +0300

    It might be used for make profit

--- core/arbitrage/__init__.py
+++ core/arbitrage/__init__.py
@@ -36,3 +36,28 @@ class Ticket(object):
 
     def __repr__(self) -> str:
         return f'[{self.ticket}]'
+
+
+class Scheme(object):
+
+    def __init__(self, path, actions) -> None:
+        self.path = path
+        self.actions = actions
+
+    def __repr__(self) -> str:
+        log = ''
+        i = 0
+        for action in self.actions:
+            if action.split()[0] in ('SELL', 'BUY'):
+                log += f'\t{self.path[i][1]} {self.path[i][0]} {action.split()[0]} : {action.split()[1]}\n'
+                i += 1
+            else:
+                log += f'\t{action}\n'
+        return log
+
+
+
+
+def criteria(worst: float, best: float, speed: float):
+    assert 0 <= speed <= 1
+    return worst*(1-speed) + best*speed
--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from core.arbitrage import ArbitrageParser, Ticket
+from core.arbitrage import ArbitrageParser, Ticket, Scheme, criteria
 from core.telegram_bot import TelegramBot
 import tqdm
 import itertools
-
+import ccxt
 
 ###
 exchanges = [
     'binance',
-    'huobi',
-    'bybit'
+    # 'huobi',
+    # 'bybit'
 ]
 
-tickets = {
-    # Hot triangle
-    'HOT/USDT',
-    'HOT/ETH',
-    'ETH/USDT',
-    # IRIS triangle
-    'IRIS/USDT',
-    'IRIS/BTC',
-    'BTC/USDT',
-    # DOT
-    'DOT/USDT',
-    'DOT/BNB',
-    'DOT/ETH',
-    'DOT/BTC',
-    # BNB
-    'BNB/USDT',
-    'BNB/BUSD'
-}
+blacklisted_crypto = {'NGN'}
+
+tickets = set(
+    list(ccxt.binance().load_markets().keys())
+) - blacklisted_crypto
+
 
 cycle_crypto = 'USDT'
 max_length_of_bundle = 3
@@ -36,7 +24,8 @@ use_only_max = True
 deposit = 100
 tax = 0.00075
 transfer_gaz = 1
-target_spead = 0.5
+target_spread = 1
+speed = 0.5
 ###
 
 def main():
@@ -50,10 +39,14 @@ def main():
 
     available_crypto = set()
     for t in tickets:
-        available_crypto.add(t.split('/')[0])
-        available_crypto.add(t.split('/')[1])
-    assert (cycle_crypto in available_crypto)
+        l, r = t.split('/')
 
+        if l in blacklisted_crypto or r in blacklisted_crypto:
+            continue
+
+        available_crypto.add(l)
+        available_crypto.add(r)
+    assert (cycle_crypto in available_crypto)
     bundles = []
     for k in range((max_length_of_bundle-1) if use_only_max else 0, max_length_of_bundle):
         for b in itertools.combinations_with_replacement(available_crypto, k):
@@ -67,11 +60,10 @@ def main():
     for _ in progress_bar:
         # 1) Parse data
         parser.parse(tickets)
-        progress_bar.set_description(
-            f'Parse time: {parser.parse_time}'
-        )
         data = parser.last_data
         reformatted_data = dict()
+
+        print('\nParsing Completed')
 
         # 2) Reformatting the data
         for type_ in data:
@@ -87,6 +79,9 @@ def main():
                         reformatted_data[Ticket(result['ticket'])] = [
                             [ticket, exchange, result['bid'], result['ask']]
                         ]
+
+        print('\nData reformatted')
+
         # 3) Analyze bundles
         max_profit = 0
         for bundle in bundles:
@@ -102,36 +97,45 @@ def main():
                 for path in all_paths:
                     profit = deposit
                     last_exchange = None
+                    actions = []
                     for i, part in enumerate(path):
                         ticket, exchange, ask, bid = part
 
                         if not((last_exchange == exchange) or (last_exchange is None)):
                             profit *= (1 - transfer_gaz/deposit)
+                            actions.append(f'TRANSFER: {last_exchange}->{exchange}')
 
                         target_base, target_quote = ticket.base, ticket.quote
                         source_base, source_quote = bundle[i].split('/')
+                        price = criteria(bid, ask, speed)
                         if target_base != source_base:
                             # /
-                            profit /= ask
+                            profit /= price
+                            actions.append(f'BUY {price:.10f}')
                         else:
                             # *
-                            profit *= bid
+                            profit *= price
+                            actions.append(f'SELL {price:.10f}')
                         # add tax
                         profit *= (1 - tax)
                     max_profit = max(max_profit, profit)
-                    if (profit/deposit-1)*100 >= target_spead:
+                    if target_spread <= (profit/deposit-1)*100:
+                        scheme = Scheme(path, actions)
                         msg = f"""
-New Arbitrage Situation:
-Scheme: {path},
+------------<New Arbitrage Situation>------------:
 ► Deposit: {deposit:.5f} {cycle_crypto},
 ◯ Total: {profit:.5f} {cycle_crypto},
 ► Spread: {(profit/deposit-1)*100:.5f} %,
 ◯ Profit: {profit-deposit:.5f} {cycle_crypto},
-"""
+►Scheme:
+{scheme}"""
                         # call telegram
                         bot.send_message(message=msg)
                         # Do log
                         print(msg)
+            progress_bar.set_description(
+                f'Parse time: {parser.parse_time}, Max profit: {max_profit}'
+            )
 
 
 
